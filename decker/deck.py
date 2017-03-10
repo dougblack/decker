592d46813d9f9c3872e2254f78c8c07a29ca24cf
Doug Black <dblack@twilio.com>
2017-03-10 00:32:43 -0800

    Fix dimensions and add hidden card support

--- decker/deck.py
+++ decker/deck.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import requests
+from io import BytesIO
 
 from decker.gatherer import Gatherer
 
@@ -13,10 +14,14 @@ CARD_HEIGHT = IMAGE_HEIGHT + 2 * Y_OFFSET
 
 
 class Deck(object):
-    def __init__(self, card_names):
+    def __init__(self, card_names, card_back_url=None, hidden_url=None):
         self.card_names = card_names
         self.gatherer = Gatherer()
+        self.hidden_url = hidden_url or 'http://media-hearth.cursecdn.com/attachments/39/665/cardback_1.png'
+        self.card_back_url = card_back_url
         self.cards = []
+        self._hidden_image = None
+        self._card_back_image = None
 
     def hydrate(self):
         for card in self.card_names:
@@ -27,7 +32,7 @@ class Deck(object):
         return self.cards
 
     def image(self):
-        deck_image = Image.new('RGB', (CARD_WIDTH * 7, CARD_HEIGHT * 10))
+        deck_image = Image.new('RGB', (CARD_WIDTH * 10, CARD_HEIGHT * 7))
         for i, card in enumerate(self.cards):
             deck_num = i // 69;
             deck_id = i % 69;
@@ -41,5 +46,23 @@ class Deck(object):
             region = (real_x, real_y)
             deck_image.paste(card.image(), region);
 
+        deck_image.paste(self.hidden_image(), (CARD_WIDTH * 9, CARD_HEIGHT * 6))
+
         self._image = deck_image
         return self._image
+
+    def hidden_image(self):
+        if self._hidden_image:
+            return self._hidden_image
+        response = requests.get(self.hidden_url)
+        self._hidden_image = Image.open(BytesIO(response.content))
+        self._hidden_image = self._hidden_image.resize((312, 445))
+        return self._hidden_image
+
+    def card_back_image(self):
+        if self._card_back_image:
+            return self._card_back_image
+        response = requests.get(self.card_back_url)
+        self._card_back_image = Image.open(BytesIO(response.content))
+        self._card_back_image = self._card_back_image.resize((312, 445))
+        return self._card_back_image
